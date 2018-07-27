b97c125619a8a55d8e6dd322878712545dc23c4b
陈磊 <yilong_cl@163.com>
2018-07-27 18:03:41 +0800

    =iii

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,52 +4,24 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    banner: [{
+      link: "",
+      image_url: "http://mz.djmall.xmisp.cn/files/logo/20161207/148110444480.jpg"
+
+    }, {
+      link: "",
+      image_url: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117973270.jpg"
+
+    }, {
+      link: "",
+      image_url: "http://mz.djmall.xmisp.cn/files/logo/20161208/148117981591.jpg"
+
+    }, {
+      link: "",
+      image_url: "http://mz.djmall.xmisp.cn/files/logo/20161212/14815381301.jpg"
+
+    }]
+
   },
   //下拉刷新
   onPullDownRefresh: function() {
