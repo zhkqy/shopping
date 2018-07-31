cd337619b4558234bac42bc4b50ae3b476000423
zhkqy <yilong_cl@163.com>
2018-08-01 07:16:18 +0800

    首页ui调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -27,6 +27,7 @@ Page({
       title: "甜橙",
       prePrice: "10元",
       price: "8元",
+      descrip: "描述内容",
       image_url: "http://image2.benlailife.com/ProductImages/000/000/037/728/big/2e8bd2d7-192b-40d9-80e3-a66a870e4178.jpg"
     }, {
       sid: "02",
@@ -34,6 +35,7 @@ Page({
       title: "孟买香蕉",
       prePrice: "10元",
       price: "8元",
+      descrip: "描述内容",
       image_url: "http://image1.benlailife.com/ProductImages/000/000/039/517/big/02854e6c-cd6e-48a3-907c-9130a6a6968d.jpg"
     }, {
       sid: "03",
@@ -41,12 +43,14 @@ Page({
       title: "泰国芒果",
       prePrice: "10元",
       price: "8元",
+      descrip: "描述内容",
       image_url: "http://img.tupianzj.com/uploads/allimg/180131/9-1P131163314.jpg"
     }, {
       sid: "04",
       isnew: true,
       title: "西红柿",
       prePrice: "10元",
+      descrip: "描述内容",
       price: "8元",
       image_url: "http://img.tupianzj.com/uploads/allimg/180131/9-1P131163314.jpg"
     }, {
@@ -54,6 +58,7 @@ Page({
       isnew: true,
       title: "火龙果",
       prePrice: "10元",
+      descrip: "描述内容",
       price: "8元",
       image_url: "http://image4.benlailife.com/ProductImages/000/000/037/727/big/37a49c73-2883-4732-8747-4d0bb74f2e4d.jpg"
     }, {
@@ -62,11 +67,13 @@ Page({
       title: "甜橙",
       prePrice: "10元",
       price: "8元",
+      descrip: "描述内容",
       image_url: "http://image2.benlailife.com/ProductImages/000/000/037/728/big/2e8bd2d7-192b-40d9-80e3-a66a870e4178.jpg"
     }, {
       sid: "02",
       isnew: true,
       title: "孟买香蕉",
+      descrip: "描述内容",
       prePrice: "10元",
       price: "8元",
       image_url: "http://image1.benlailife.com/ProductImages/000/000/039/517/big/02854e6c-cd6e-48a3-907c-9130a6a6968d.jpg"
@@ -75,12 +82,14 @@ Page({
       isnew: true,
       title: "泰国芒果",
       prePrice: "10元",
+      descrip: "描述内容",
       price: "8元",
       image_url: "http://img.tupianzj.com/uploads/allimg/180131/9-1P131163314.jpg"
     }, {
       sid: "04",
       isnew: true,
       title: "西红柿",
+      descrip: "描述内容",
       prePrice: "10元",
       price: "8元",
       image_url: "http://img.tupianzj.com/uploads/allimg/180131/9-1P131163314.jpg"
@@ -89,11 +98,13 @@ Page({
       isnew: true,
       title: "火龙果",
       prePrice: "10元",
+      descrip: "描述内容",
       price: "8元",
       image_url: "http://image4.benlailife.com/ProductImages/000/000/037/727/big/37a49c73-2883-4732-8747-4d0bb74f2e4d.jpg"
     }, {
       sid: "04",
       isnew: true,
+      descrip: "描述内容",
       title: "火龙果",
       prePrice: "10元",
       price: "8元",
